fa20b97fc4b81e75f0844f09aae5b4ec3ec1279f
demarexhep <60626388+demarexhep@users.noreply.github.com>
2020-09-20 14:31:35 +0200

    portfolio

---
+++ build/precache-manifest.a80aef8428654276a000850e443c8669.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea19741931cc79952a7e91a0be145681",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6e60917b9328effd1ee",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f29ece731f21b2276a11",
+    "url": "/static/js/2.e4e08dad.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.e4e08dad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6e60917b9328effd1ee",
+    "url": "/static/js/main.426977af.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
